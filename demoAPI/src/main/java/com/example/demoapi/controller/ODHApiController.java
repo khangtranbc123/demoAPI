2aed2e9ec97db84fbdd373f2806050730313e964
Canh Nguyen <leagueyasuo186@gmail.com>
2022-11-23 23:16:36 +0700

    done

--- demoAPI/src/main/java/com/example/demoapi/controller/ODHApiController.java
+++ demoAPI/src/main/java/com/example/demoapi/controller/ODHApiController.java
@@ -1,21 +1,24 @@
 package com.example.demoapi.controller;
-
+import com.example.demoapi.model.Request;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/api")
 public class ODHApiController {
     @PostMapping("/getODH")
-    public String getVui(@RequestBody String p_cifinfo){
+    public ResponseEntity<String> getVui(@RequestBody Request request){
         String id1 =
-                "{"
-                +"\"p_cifinfo\":\"01791798\""
-                +"}";
+                "01791798";
+
         String ok;
-//        String data = p_cifinfo.replaceAll(" ", "");
-//        System.out.println(p_cifinfo);
-//        System.out.println(data);
-        if(p_cifinfo.equals(id1)){
+       String data = request.getP_cifinfo().replaceAll(" ", "");
+        if(data.equals(id1)){
             ok = "{"
                     + "\"results\":"
                     + "{"
@@ -26,11 +29,41 @@ public class ODHApiController {
                     + ",\"industry_code\":\"CAC H.DONG VA DICH VU KHAC\""
                     + "}"
                     + "}";
-            return ok;
+            return new ResponseEntity<>(ok, HttpStatus.OK);
         }
         else {
             ok = "[]";
-            return ok;
+            return new ResponseEntity<>(ok, HttpStatus.OK);
         }
     }
+
+//    @PostMapping("/getODH")
+//    public ResponseEntity<String> getVui(@RequestBody Request request, @RequestHeader(value = "authorize") String value){
+//        HttpHeaders headers1 = new HttpHeaders();
+//        HttpHeaders headers2 = new HttpHeaders();
+//        headers1.set("authorize", "IM80gqkhsVRloftQcJvajg==");
+//        headers2.set("authorize", value);
+//        String id1 =
+//                "01791798";
+//
+//        String ok;
+//        String data = request.getP_cifinfo().replaceAll(" ", "");
+//        if(data.equals(id1) && headers1.equals(headers2)){
+//            ok = "{"
+//                    + "\"results\":"
+//                    + "{"
+//                    + "\"cif\":\"01275299\""
+//                    + ",\"dkkd\":\"01275299\""
+//                    + ",\"mst\":\"null\""
+//                    + ",\"industry\":\"179900\""
+//                    + ",\"industry_code\":\"CAC H.DONG VA DICH VU KHAC\""
+//                    + "}"
+//                    + "}";
+//            return new ResponseEntity<>(ok, HttpStatus.OK);
+//        }
+//        else {
+//            ok = "[]";
+//            return new ResponseEntity<>(ok, HttpStatus.OK);
+//        }
+//    }
 }
