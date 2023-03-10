be492cc284f23d6f54085bea2c077b54cb9a94cd
CanhNguyen <thecanh.nguyen@vmgmedia.vn>
2023-03-10 09:25:59 +0700

    done

--- demoAPI/src/main/java/com/example/demoapi/controller/ODHApiController.java
+++ demoAPI/src/main/java/com/example/demoapi/controller/ODHApiController.java
@@ -1,35 +1,38 @@
 package com.example.demoapi.controller;
 import com.example.demoapi.model.Request;
+import com.example.demoapi.model.ResultTotal;
+import com.example.demoapi.model.resultODH.ResultAr;
+import com.example.demoapi.model.resultODH.ResultCts;
+import com.example.demoapi.model.resultODH.ResultODHar;
+import com.example.demoapi.model.resultODH.ResultODHcts;
+import com.example.demoapi.model.results;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.DecimalFormat;
 import java.util.HashMap;
 
 @RestController
 @RequestMapping("/wisd-rest2/DataService")
 public class ODHApiController {
     @PostMapping("/ODH_SERVICES/cst_industry")
-    public ResponseEntity<String> getVui(@RequestBody Request request){
+    public ResponseEntity<?> getVui(@RequestBody Request request){
         String id =
                 "01791798";
 
         String ok;
-       String data = request.getP_cifinfo().replaceAll(" ", "");
-        if(data.equals(id)){
-            ok = "{"
-                    + "\"results\":"
-                    + "{"
-                    + "\"cif\":\"01275299\""
-                    + ",\"dkkd\":\"01275299\""
-                    + ",\"mst\":null"
-                    + ",\"industry\":\"179900\""
-                    + ",\"industry_code\":\"CAC H.DONG VA DICH VU KHAC\""
-                    + "}"
-                    + "}";
-            return new ResponseEntity<>(ok, HttpStatus.OK);
+        if((id.equals(request.getP_cifinfo()) || request.getP_cifinfo() == null) && (id.equals(request.getP_unq_id()) || request.getP_unq_id() == null) && (id.equals(request.getP_udf5()) || request.getP_udf5() == null) ) {
+            results result = new results();
+            result.setCif("01275299");
+            result.setDkkd("01275299");
+            result.setIndustry("179900");
+            result.setIndustry_code("CAC H.DONG VA DICH VU KHAC");
+            ResultTotal results = new ResultTotal();
+            results.setResults(result);
+            return new ResponseEntity<>(results, HttpStatus.OK);
         }
         else {
             ok = "[]";
@@ -38,25 +41,15 @@ public class ODHApiController {
     }
 
     @PostMapping("/ODH_SERVICES/cst_ar_info_001")
-    public ResponseEntity<String> getArInfo(@RequestBody Request request){
+    public ResponseEntity<?> getArInfo(@RequestBody Request request){
         String id =
                 "02501168";
-
         String ok;
-        String data = request.getP_cifinfo().replaceAll(" ", "");
-        if(data.equals(id)){
-            ok = "{"
-                    + "\"results\":"
-                    + "{"
-                    + "\"cif\":\"02501168\""
-                    + ",\"dkkd\":\"02501168\""
-                    + ",\"mst\": null"
-                    + ",\"duno_sanpham\":\"02501168\""
-                    + ",\"duno_branch\":\"021\""
-                    + ",\"duno_osamount\": 959540.00000"
-                    + "}"
-                    + "}";
-            return new ResponseEntity<>(ok, HttpStatus.OK);
+        if((id.equals(request.getP_cifinfo()) || request.getP_cifinfo() == null) && (id.equals(request.getP_unq_id()) || request.getP_unq_id() == null) && (id.equals(request.getP_udf5()) || request.getP_udf5() == null) ){
+            ResultODHar result = new ResultODHar("02501168", "02501168", null, "02501168", "021",959540.00000);
+            ResultAr results = new ResultAr();
+            results.setResults(result);
+            return new ResponseEntity<>(results, HttpStatus.OK);
         }
         else {
             ok = "[]";
@@ -65,25 +58,15 @@ public class ODHApiController {
     }
 
     @PostMapping("/DMT_SERVICES/cst_info_001")
-    public ResponseEntity<String> getCtsInfo(@RequestBody Request request){
-        String id =
-                "01791798";
+    public ResponseEntity<?> getCtsInfo(@RequestBody Request request){
+        String id = "01791798";
 
         String ok;
-        String data = request.getP_cifinfo().replaceAll(" ", "");
-        if(data.equals(id)){
-            ok = "{"
-                    + "\"results\":"
-                    + "{"
-                    + "\"cif\":\"01791798\""
-                    + ",\"dkkd\":\"******158\""
-                    + ",\"mst\": null"
-                    + ",\"cusdebtstatus\": null"
-                    + ",\"maxdebtgroup\": null"
-                    + ",\"non_fin5\": 999999999.0000000000"
-                    + "}"
-                    + "}";
-            return new ResponseEntity<>(ok, HttpStatus.OK);
+        if((id.equals(request.getP_cifinfo()) || request.getP_cifinfo() == null) && (id.equals(request.getP_unq_id()) || request.getP_unq_id() == null) && (id.equals(request.getP_udf5()) || request.getP_udf5() == null) ){
+            ResultODHcts result = new ResultODHcts("01791798", "******158", null, null, null, 999999999.0000000000);
+            ResultCts results = new ResultCts();
+            results.setResults(result);
+            return new ResponseEntity<>(results, HttpStatus.OK);
         }
         else {
             ok = "[]";
--- demoAPI/src/main/java/com/example/demoapi/model/Request.java
+++ demoAPI/src/main/java/com/example/demoapi/model/Request.java
@@ -2,6 +2,30 @@ package com.example.demoapi.model;
 
 public class Request {
     String p_cifinfo;
+    String p_unq_id;
+    String p_udf5;
+
+    public Request(String p_cifinfo, String p_unq_id, String p_udf5) {
+        this.p_cifinfo = p_cifinfo;
+        this.p_unq_id = p_unq_id;
+        this.p_udf5 = p_udf5;
+    }
+
+    public String getP_unq_id() {
+        return p_unq_id;
+    }
+
+    public void setP_unq_id(String p_unq_id) {
+        this.p_unq_id = p_unq_id;
+    }
+
+    public String getP_udf5() {
+        return p_udf5;
+    }
+
+    public void setP_udf5(String p_udf5) {
+        this.p_udf5 = p_udf5;
+    }
 
     public Request(String p_cifinfo) {
         this.p_cifinfo = p_cifinfo;
